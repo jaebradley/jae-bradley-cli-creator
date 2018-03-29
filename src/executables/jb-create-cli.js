532f97fd1ec0b7774b03676058183d9f5be4b2b6
jaebradley <jae.b.bradley@gmail.com>
2018-03-29 09:06:14 -0700

    feat(create-command): remove the create command
    BREAKING CHANGE: The create command is superfluous and unnecessary
    
    BREAKING CHANGE: The create command is superfluous and unnecessary

--- src/executables/jb-create-cli.js
+++ src/executables/jb-create-cli.js
@@ -3,8 +3,19 @@
 import program from 'commander';
 
 import pkg from '../../package.json';
+import createPackage from '../createPackage';
+
+const execute = async () => {
+  try {
+    await createPackage();
+  } catch (e) {
+    console.error('😞  Rut ro, an error occurred');
+    console.error(e);
+  }
+};
 
 program.version(pkg.version)
   .description('CLI that creates starting point for CLI npm packages')
-  .command('create', 'Create a CLI')
   .parse(process.argv);
+
+execute();
